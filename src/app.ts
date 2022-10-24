a96c313e7a40066553fc80b2746f4aef8907a0d0
Luca Modica <luca.modica@di.unito.it>
2022-10-25 00:12:56 +0200

    test "on video note" command

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,27 @@
 import { Context, Markup, Telegraf, Telegram } from 'telegraf';
 import { Update } from 'typegram';
 
+// bot init
 const token: string = process.env.BOT_TOKEN as string;
 const bot: Telegraf<Context<Update>> = new Telegraf(token);
 const telegram: Telegram = new Telegram(token);
-const chatId: string = process.env.CHAT_ID as string;
+// const chatId: string = process.env.CHAT_ID as string;
 
 
 // bot handlers
+// /start
 bot.start((ctx) => {
   ctx.reply('Hello ' + ctx.from.first_name + '!');
 });
 
+// /help
 bot.help((ctx) => {
   ctx.reply('Send /start to receive a greeting');
   ctx.reply('Send /keyboard to receive a message with a keyboard');
   ctx.reply('Send /quit to stop the bot');
 });
 
+// /quit
 bot.command('quit', (ctx) => {
   // Explicit usage
   ctx.telegram.leaveChat(ctx.message.chat.id);
@@ -26,35 +30,15 @@ bot.command('quit', (ctx) => {
   ctx.leaveChat();
 });
 
-bot.command('keyboard', (ctx) => {
-  ctx.reply(
-    'Keyboard',
-    Markup.inlineKeyboard([
-      Markup.button.callback('First option', 'first'),
-      Markup.button.callback('Second option', 'second'),
-      Markup.button.callback('Third option', 'third'),
-    ])
-  );
-});
-
-bot.on('text', (ctx) => {
-  ctx.reply(
-    'You choose the ' +
-      (ctx.message.text === 'first' ? 'First' : 'Secondddd') +
-      ' Option!'
-  );
-
-  if (chatId) {
-    telegram.sendMessage(
-      chatId,
-      'This message was sent without your interaction!'
-    );
-  }
+// doing something on receiving a video note
+bot.on('video_note', (ctx) => {
+  ctx.reply(`The video message`);
 });
 
 
 // bot starting point
 bot.launch();
+
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
